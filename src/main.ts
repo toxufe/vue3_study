455746b0e8e3e5337ac9bdb9095b4a099707aec2
Fly <723887809@qq.com>
2024-01-03 08:57:50 +0800

    Add vcolorpicker library and update main.ts and App.vue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Tree from './components/Tree.vue'
+import vcolorpicker from 'vcolorpicker'
+import 'vcolorpicker/lib/style.css'
 
 const app = createApp(App);
 app.component('Tree', Tree);
+
+app.use(vcolorpicker);
 
 app.mount('#app')

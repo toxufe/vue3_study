fa7db49fafc3903d7bff2bd180ddff721fb9ea05
Fly <723887809@qq.com>
2024-01-05 17:04:38 +0800

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,13 @@
 <template>
-    <h1>v-model 知识点 父组件</h1>
+    <h1>自定义指令</h1>
 
-    <!-- v-model vue2 value vue3  -->
-    <!-- vue3 中支持绑定多个v-model -->
-    <!-- v-model 的修饰符 v-model:name.isbt nameModifiers?:{
-        isbt
-    }  接受规范 -->
+    <!-- 
+        破坏性更新
+     -->
 
-    <div>Flat:{{ flag }}</div>
-    <div>Name: {{name}}</div>
-
-    <button @click="flag=!flag">更改啊</button>
-
-    <hr />
-
-    <h2>子组件</h2>
-    <A v-model:name.isbt="name" v-model="flag"></A>
+  
 </template>
 <script setup lang="ts">
-import A from "@/tsx/A";
-let flag = ref<boolean>(false);
-let name = ref<string>("张三");
+
 </script>
 <style scoped lang="scss"></style>

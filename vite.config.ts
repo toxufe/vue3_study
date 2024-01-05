3bd739a77a2df6b03332477b51318e0f334c3361
Fly <723887809@qq.com>
2024-01-05 09:37:09 +0800

    update 5 files

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vitetsx from './plugins/index'
-// import vueJsx from '@vitejs/plugin-vue-jsx'
+// import vitetsx from './plugins/index'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // vueJsx(),
-    vitetsx()
+    vueJsx(),
+    AutoImport({
+      imports: ['vue'],
+    })
+    // vitetsx()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  css:{
-    preprocessorOptions:{
-      scss:{
+  css: {
+    preprocessorOptions: {
+      scss: {
         additionalData: `@import "./src/bem.scss";`
       }
     }

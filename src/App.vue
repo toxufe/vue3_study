a87c40006842da112d94fa78d39636ee54bcb867
Fly <723887809@qq.com>
2024-01-09 17:01:21 +0800

    build(deps): update package-lock.json, package.json, App.vue and main.ts

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-    <h1>自定义hook和自定义指定 封装npm库</h1>
-    <div v-resize="xxxx" class="div"></div>
+    <h1 v-dark>自定义hook和自定义指定 封装npm库</h1>
+    <!-- <div v-resize="xxxx" class="div"></div> -->
 </template>
 <script setup lang="ts">
+// import useDark from 'v-usedark-fly';
+// import { onMounted } from "vue";
+
+
+// onMounted(() => {
+//     useDark()
+// });
 // import { onMounted } from "vue";
 // import useResize from "v-resize-fly";
 
@@ -11,16 +18,21 @@
 //         console.log(size);
 //     })
 // });
-const xxxx = (size:any) => {
-    console.log("-------",size);
-};
+// const xxxx = (size:any) => {
+//     console.log("-------",size);
+// };
 </script>
-<style scoped lang="scss">
+<style lang="scss">
 .div {
     width: 200px;
     height: 200px;
     border: 3px solid #ccc;
     resize: both;
     overflow: hidden;
+}
+
+body {
+    background: #f00;
+    color: yellow;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import Tree from './components/Tree.vue'
 // import vcolorpicker from 'vcolorpicker'
 // import 'vcolorpicker/lib/style.css'
 import useResize from "v-resize-fly";
+import useDark from 'v-usedark-fly';
+
 
 const app = createApp(App);
 app.component('Tree', Tree);
 
 app.use(useResize);
+app.use(useDark);
 app.mount('#app')

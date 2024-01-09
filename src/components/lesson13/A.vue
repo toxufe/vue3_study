a935d564ac082f4f02165fc7842138a6dbb12df6
Fly <723887809@qq.com>
2024-01-09 09:07:47 +0800

    update auto-imports.d.ts and App.vue

---
+++ src/components/lesson13/A.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {} from "vue"
+</script>
+
+<template>
+  <div class="box">我是组件A</div>
+</template>
+
+<style lang="scss" scoped>
+
+.box {
+    width: 200px;
+    height: 200px;
+    border: 1px solid #ccc;
+}
+</style>

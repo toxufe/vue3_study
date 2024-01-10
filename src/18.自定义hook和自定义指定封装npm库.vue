fc508a676937d98bb0e8d12b0b7829ab0b707d21
Fly <723887809@qq.com>
2024-01-10 08:06:18 +0800

    update App.vue

---
+++ src/18.自定义hook和自定义指定封装npm库.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>自定义hook和自定义指定 封装npm库</h1>
+    <!-- <div v-resize="xxxx" class="div"></div> -->
+</template>
+<script setup lang="ts">
+import useDark from 'v-usedark-fly';
+import { onMounted } from "vue";
+
+
+onMounted(() => {
+    useDark()
+});
+// import { onMounted } from "vue";
+// import useResize from "v-resize-fly";
+
+// onMounted(() => {
+//     useResize(document.querySelector(".div") as HTMLElement,(size:any)=>{
+//         console.log(size);
+//     })
+// });
+// const xxxx = (size:any) => {
+//     console.log("-------",size);
+// };
+</script>
+<style lang="scss" scoped>
+.div {
+    width: 200px;
+    height: 200px;
+    border: 3px solid #ccc;
+    resize: both;
+    overflow: hidden;
+}
+
+body {
+    background: #f00;
+    color: yellow;
+}
+</style>

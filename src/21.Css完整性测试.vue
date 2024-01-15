e4b47a6986e53e5e58e5c4e2046b54fcb90bcb5c
Fly <723887809@qq.com>
2024-01-15 16:46:59 +0800

    update 5 files

---
+++ src/21.Css完整性测试.vue
@@ -0,0 +1,46 @@
+<template>
+    <h1>css 完成新特性</h1>
+
+    <!-- <div :class="$style.div">
+
+    </div> -->
+
+
+    <!-- <div :class="xxx.div">
+        你好世界
+
+</div> -->
+
+</template>
+<script setup lang="ts">
+// import {ref} from 'vue'
+// const style = ref('red');
+// const style01 = ref({
+//     color :'red'
+// })
+</script>
+<style module="xxx">
+    .div {
+        color: red;
+    }
+</style>
+<style lang="scss" scoped>
+// // 插槽选择器 
+// :slotted(.red){
+//     color: red;
+// }
+// // 全局选择器
+// :global(.red){
+//     color: pink;
+// }
+// // 动态css
+// .red {
+//     color: v-bind(style);
+// }
+
+// .red {
+//     color: v-bind('style01.color');
+// }
+</style>
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-    
-
+    <h1 class="bg-teal-600 text-white">vue3继承 Tainwind CSS</h1>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
 <style lang="scss" scoped></style>
-
-

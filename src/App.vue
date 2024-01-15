35d11df23973596e8026a8338f51f64c9fc3ee23
Fly <723887809@qq.com>
2024-01-15 16:54:36 +0800

    update App.vue and src/index.css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
+    <!-- https://www.tailwindcss.cn/docs/editor-setup -->
     <h1 class="bg-teal-600 text-white">vue3继承 Tainwind CSS</h1>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+   
+
+
+    <button class="btn btn--secondary">Decline</button>
+    <button class="btn btn--primary">Accept</button>
 </template>
 <script setup lang="ts"></script>
 

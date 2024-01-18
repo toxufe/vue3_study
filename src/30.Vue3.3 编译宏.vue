cb5400720526c9e0ee25e5222d53bfe512973b5d
Fly <723887809@qq.com>
2024-01-18 14:08:35 +0800

    Update Vue3 environment variables and Vite configuration

---
+++ src/30.Vue3.3 编译宏.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Vue3.3 编译宏</h1>
+
+
+<!-- 
+
+    vue 3.3 新增了  defineProps 新增了泛型的支持 用于类型推断
+    generic type inference for defineProps
+ -->
+
+ <xm @getname="xxx" :name="['a','b','c']">
+ <template #default="{item,index}">
+{{item}} === {{index}}
+</template>
+ </xm>
+</template>
+<script setup lang="ts">
+import xm from '@/components/lession30/child.vue'
+const xxx  = (name:string) => {
+  console.log(name);
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import vitetsx from './plugins/index'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -8,43 +8,48 @@ import AutoImport from 'unplugin-auto-import/vite'
 import {PostCssPxToViewport} from './plugins/postcss-px-to-viewport'
 import unoCss from 'unocss/vite'
 import {presetIcons,presetAttributify,presetUno} from 'unocss'
+import { log } from 'node:console'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      imports: ['vue'],
-    }),
-    unoCss({
-      presets:[presetIcons(),presetAttributify(),presetUno()],
-      rules:[
-        [/^m-(\d+)$/, ([, d]) => ({ margin: `${Number(d) * 10}px` })],
-        ['red',{color:'red'}],
-        ['f40',{"font-size":'40px'}],
-      ],
-      shortcuts:{
-        cike:['red']
+export default ({mode}:any)=>{
+  console.log('mode',loadEnv(mode,process.cwd()));
+  
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        imports: ['vue'],
+      }),
+      unoCss({
+        presets:[presetIcons(),presetAttributify(),presetUno()],
+        rules:[
+          [/^m-(\d+)$/, ([, d]) => ({ margin: `${Number(d) * 10}px` })],
+          ['red',{color:'red'}],
+          ['f40',{"font-size":'40px'}],
+        ],
+        shortcuts:{
+          cike:['red']
+        }
+      })
+      // vitetsx()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    })
-    // vitetsx()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    postcss: {
-      plugins: [
-        // PostCssPxToViewport()
-      ]
     },
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/bem.scss";`
+    css: {
+      postcss: {
+        plugins: [
+          // PostCssPxToViewport()
+        ]
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/bem.scss";`
+        }
       }
     }
-  }
-})
+  })
+}

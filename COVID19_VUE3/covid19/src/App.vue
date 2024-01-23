65136754366953133eb6150b55cb63217ec308d0
Fly <723887809@qq.com>
2024-01-23 11:22:55 +0800

    build(deps): update package-lock.json, package.json, App.vue and COVID19_VUE3/covid19/src/stores/index.ts

--- COVID19_VUE3/covid19/src/App.vue
+++ COVID19_VUE3/covid19/src/App.vue
@@ -5,6 +5,7 @@ import { useStore } from "./stores";
 import * as echarts from "echarts";
 import "@/assets/china.js";
 import { geoCoordMap } from "@/assets/geoMaps";
+import "animate.css";
 
 const store = useStore();
 
@@ -127,7 +128,7 @@ const initCharts = () => {
         ],
     };
 
-    const charts = echarts.init(document.querySelector("#china") as HTMLElement,null,{ height: 800});
+    const charts = echarts.init(document.querySelector("#china") as HTMLElement, null, { height: 800 });
     charts.setOption(option);
 
     // 点击事件
@@ -136,18 +137,157 @@ const initCharts = () => {
     });
 };
 
+const initPie = () => {
+    const charts = echarts.init(document.querySelector(".box-left-pie") as HTMLElement);
+    const option = {
+        backgroundColor: "#223651",
+        tooltip: {
+            trigger: "item",
+        },
+        // legend: {
+        //   top: '5%',
+        //   left: 'center'
+        // },
+        series: [
+            {
+                // name: 'Access From',
+                type: "pie",
+                radius: ["40%", "70%"],
+                avoidLabelOverlap: true,
+                itemStyle: {
+                    borderRadius: 4,
+                    borderColor: "#fff",
+                    borderWidth: 2,
+                },
+                label: {
+                    show: true,
+                    // position: 'center'
+                },
+                emphasis: {
+                    label: {
+                        show: true,
+                        fontSize: 15,
+                        // fontWeight: 'bold'
+                    },
+                },
+                // labelLine: {
+                //   show: false
+                // },
+                data: store.chinaDayList.map((v) => {
+                    return {
+                        name: v.date,
+                        value: v.today.confirm,
+                    };
+                }),
+            },
+        ],
+    };
+    charts.setOption(option);
+};
+const initLine = () => {
+    const charts = echarts.init(document.querySelector(".box-left-line") as HTMLElement);
+    const option = {
+        backgroundColor: "#223651",
+        xAxis: {
+            type: "category",
+            data: store.chinaDayList.map((v) => v.date),
+            axisLine: {
+                lineStyle: {
+                    color: "#fff",
+                },
+            },
+        },
+        yAxis: {
+            type: "value",
+            axisLine: {
+                lineStyle: {
+                    color: "#fff",
+                },
+            },
+        },
+        label: { show: true },
+        tooltip: {
+            trigger: "axis",
+        },
+        series: [
+            {
+                data: store.chinaDayList.map((v) => v.today.confirm),
+                type: "line",
+                smooth: true,
+            },
+        ],
+    };
+    charts.setOption(option);
+};
 onMounted(async () => {
     await store.getList();
     initCharts();
+    initPie();
+    initLine();
 });
 </script>
 
 <template>
     <div class="box" :style="{ background: `url(${bg})` }">
-        <div class="box-left"></div>
-        <div id="china" class="box-center">
+        <div class="box-left">
+            <div class="box-left-card">
+                <section>
+                    <div>较上日+ {{ store.chinaAdd.storeConfirm % 10 }}</div>
+                    <div>{{ store.chinaTotal.confirm }}</div>
+                    <div>本土现有确诊</div>
+                </section>
+                <section>
+                    <div>较上日+ {{ store.chinaAdd.storeConfirm % 10 }}</div>
+                    <div>{{ store.chinaTotal.confirm }}</div>
+                    <div>现有确诊</div>
+                </section>
+                <section>
+                    <div>较上日+ {{ store.chinaAdd.confirm % 10 }}</div>
+                    <div>{{ store.chinaTotal.confirm }}</div>
+                    <div>累计确诊</div>
+                </section>
+                <section>
+                    <div>较上日+ {{ store.chinaAdd.heal }}</div>
+                    <div>{{ store.chinaTotal.heal }}</div>
+                    <div>无症状感染者</div>
+                </section>
+                <section>
+                    <div>较上日+ {{ store.chinaAdd.input }}</div>
+                    <div>{{ store.chinaTotal.input }}</div>
+                    <div>境外输入</div>
+                </section>
+                <section>
+                    <div>较上日+ {{ store.chinaAdd.dead }}</div>
+                    <div>{{ store.chinaTotal.dead }}</div>
+                    <div>累计死亡</div>
+                </section>
+            </div>
+            <div class="box-left-pie"></div>
+            <div class="box-left-line"></div>
         </div>
-        <div class="box-right"></div>
+        <div id="china" class="box-center"></div>
+        <div class="box-right">
+            <table class="table" cellspacing="0" border="1">
+                <thead>
+                    <tr>
+                        <th>地区</th>
+                        <th>新增确诊</th>
+                        <th>累计确诊</th>
+                        <th>治愈</th>
+                        <th>死亡</th>
+                    </tr>
+                </thead>
+                <transition-group enter-active-class="animate__animated animate__flipInY" tag="tbody">
+                    <tr :key="item.id" v-for="(item, index) in store.item">
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.today.confirm }}</td>
+                        <td>{{ item.total.confirm }}</td>
+                        <td>{{ item.total.heal }}</td>
+                        <td>{{ item.total.dead }}</td>
+                    </tr>
+                </transition-group>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -156,6 +296,29 @@ onMounted(async () => {
     margin: 0;
     padding: 0;
 }
+
+$itemColor: #41b0db;
+$itemBg: #223651;
+$itemBorder: #212028;
+
+.table {
+    width: 100%;
+    background: #212028;
+
+    tr {
+        th {
+            padding: 5px;
+            white-space: nowrap;
+        }
+
+        td {
+            padding: 5px 10px;
+            width: 100px;
+            white-space: nowrap;
+            text-align: center;
+        }
+    }
+}
 html,
 body,
 #app {
@@ -163,12 +326,47 @@ body,
     overflow: hidden;
 }
 .box {
-    display: flex;
     height: 100%;
+    display: flex;
     overflow: hidden;
+    background-size: cover;
+    color: #fff;
     &-left {
         width: 400px;
+
+        &-pie {
+            height: 320px;
+            margin-top: 20px;
+        }
+
+        &-line {
+            height: 320px;
+            margin-top: 20px;
+        }
+
+        &-card {
+            display: grid;
+            grid-template-columns: auto auto auto;
+            grid-template-rows: auto auto;
+
+            section {
+                background: $itemBg;
+                border: 1px solid $itemBorder;
+                padding: 10px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+
+                div:nth-child(2) {
+                    color: $itemColor;
+                    padding: 10px 0;
+                    font-size: 20px;
+                    font-weight: bold;
+                }
+            }
+        }
     }
+
     &-center {
         flex: 1;
         display: flex;
--- COVID19_VUE3/covid19/src/stores/index.ts
+++ COVID19_VUE3/covid19/src/stores/index.ts
@@ -5,11 +5,18 @@ import type { RootObject, ChinaDayList, ChinaAdd, ChinaTotal } from './type'
 
 export const useStore = defineStore('store', () => {
   const list = ref<RootObject>()
+  const item = ref(<ChinaDayList[]>[])
+  const chinaAdd = ref(<ChinaAdd>{})
+  const chinaTotal = ref(<ChinaTotal>{})
+  const chinaDayList = ref(<ChinaDayList[]>[]);
 
   const getList = async () => {
     const result = await getApiList();
     list.value = result.data.data;
+    chinaAdd.value = result.data.data.chinaTotal.today;
+    chinaTotal.value = result.data.data.chinaTotal.total;
+    chinaDayList.value = result.data.data.chinaDayList.splice(0, 10);
   }
 
-  return { list, getList }
+  return { list,item,chinaAdd,chinaTotal,chinaDayList, getList }
 })

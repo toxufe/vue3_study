8862cacb330f25188e22b15e73c5665c6d7a31e8
Fly <723887809@qq.com>
2024-01-23 09:41:32 +0800

    update COVID19_NODE/index.ts

---
+++ COVID19_NODE/index.ts
@@ -0,0 +1,24 @@
+import express, { Express, Router, Request, Response } from 'express'
+import axios from 'axios'
+const app: Express = express()
+
+
+// router 是用来分模块的
+// node.js 中的 express 的Router路由的作用，为了方便我们更好的管理我们的路由，把不同的路由分离到不同的文件中，方便管理和维护
+const router: Router = express.Router()
+
+app.use('/api', router); // 用来使用中间件
+
+router.get('/list', async (req: Request, res: Response) => {
+    let result = await axios.get('https://c.m.163.com/ug/api/wuhan/app/data/list-total?t=341182141440')
+
+    res.json({
+        data: result.data
+    });
+
+});
+
+let port = 3000;
+app.listen(port, () => {
+    console.log(`server is running!!! http://localhost:${port}`);
+});
